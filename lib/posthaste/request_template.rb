925e85639fbd2f799022a3dc11636df4a519ad8c
Joel Helbling <joel@joelhelbling.com>
2018-05-31 23:10:31 -0400

    improves specs for request template

--- lib/posthaste/request_template.rb
+++ lib/posthaste/request_template.rb
@@ -2,29 +2,39 @@ module Posthaste
   class RequestTemplate
     attr_reader :name, :http_method, :endpoint, :body_template
 
-    def initialize(config)
+    VALID_METHODS = %w[
+      GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE PATCH
+    ]
+
+    def initialize(config={})
       @name = config['name']
-      @http_method = config['method']
-      @endpoint = config['endpoint'] || '/'
-      @parameter_defaults = config['parameter_defaults']
-      @required_parameters = config['required_parameters']
-      @body_template = config['body_template'] || ""
+      @http_method = get_valid_method_from(config['method'])
+      @endpoint = config.fetch('endpoint', '/')
+      @parameter_defaults = config.fetch('parameter_defaults', {})
+      @required_parameters = config.fetch('required_parameters', {})
+      @body_template = config.fetch('body_template', '')
     end
 
     def header_defaults
-      @parameter_defaults&.fetch('header', {})
+      @parameter_defaults.fetch('header', {})
     end
 
     def query_string_required
-      @required_parameters&.fetch('query_string', [])
+      @required_parameters.fetch('query_string', [])
     end
 
     def header_required
-      @required_parameters&.fetch('header', [])
+      @required_parameters.fetch('header', [])
     end
 
     def body_required
-      @required_parameters&.fetch('body', [])
+      @required_parameters.fetch('body', [])
+    end
+
+    private
+
+    def get_valid_method_from(method)
+      VALID_METHODS.include?(method) ? method : 'GET'
     end
   end
 end
--- spec/posthaste/request_template_spec.rb
+++ spec/posthaste/request_template_spec.rb
@@ -2,22 +2,185 @@ require 'yaml'
 
 RSpec.describe Posthaste::RequestTemplate do
   Given(:yaml_file) { './spec/fixtures/example_request_template.yml' }
-  Given(:yaml) { YAML.load(File.read(yaml_file)) }
-  Given(:subject) { described_class.new yaml }
+  Given(:yaml) do
+    YAML.load(<<~YAML)
+      name: create article
+      method: POST
+      endpoint: /posts
+      parameter_defaults:
+        header:
+          Content-Type: application/json
+      required_parameters:
+        query_string:
+          - author
+          - topic
+        header:
+          - Authorization
+        body:
+          - title
+          - text
+      body_template: |-
+        {
+          "article_title": "<%= title %>",
+          "article_body": "<%= text %>"
+        }
+    YAML
+  end
 
-  Then { subject.name == 'create article' }
-  Then { subject.http_method == 'POST' }
-  Then { subject.endpoint == '/posts' }
-  Then { subject.header_defaults == { 'Content-Type' => 'application/json' } }
-  Then { subject.query_string_required == ['author', 'topic'] }
-  Then { subject.header_required == ['Authorization'] }
-  Then { subject.body_required == ['title', 'text'] }
-  Then do
-    expect(subject.body_template).to eq(<<~JSON.chomp)
-      {
-        "article_title": "<%= title %>",
-        "article_body": "<%= text %>"
-      }
-    JSON
+  When(:subject) { described_class.new yaml }
+
+  describe '#name' do
+    Then { subject.name == 'create article' }
+  end
+
+  describe '#http_method' do
+    context 'with GET' do
+      Given { yaml['method'] = 'GET' }
+      Then { subject.http_method == 'GET' }
+    end
+
+    context 'with HEAD' do
+      Given { yaml['method'] = 'HEAD' }
+      Then { subject.http_method == 'HEAD' }
+    end
+
+    context 'with POST' do
+      Then { subject.http_method == 'POST' }
+    end
+
+    context 'with PUT' do
+      Given { yaml['method'] = 'PUT' }
+      Then { subject.http_method == 'PUT' }
+    end
+
+    context 'with DELETE' do
+      Given { yaml['method'] = 'DELETE' }
+      Then { subject.http_method == 'DELETE' }
+    end
+
+    context 'with CONNECT' do
+      Given { yaml['method'] = 'CONNECT' }
+      Then { subject.http_method == 'CONNECT' }
+    end
+
+    context 'with OPTIONS' do
+      Given { yaml['method'] = 'OPTIONS' }
+      Then { subject.http_method == 'OPTIONS' }
+    end
+
+    context 'with TRACE' do
+      Given { yaml['method'] = 'TRACE' }
+      Then { subject.http_method == 'TRACE' }
+    end
+
+    context 'with PATCH' do
+      Given { yaml['method'] = 'PATCH' }
+      Then { subject.http_method == 'PATCH' }
+    end
+
+    context 'with an illegal method' do
+      Given { yaml['method'] = 'DANCE' }
+      Then { subject.http_method == 'GET' }
+    end
+
+    context 'with nil' do
+      Given { yaml.delete 'method' }
+      Then { subject.http_method == 'GET' }
+    end
+  end
+
+  describe '#endpoint' do
+    context 'with endpoint defined' do
+      Then { subject.endpoint == '/posts' }
+    end
+
+    context 'with default' do
+      Given { yaml.delete 'endpoint' }
+      Then { subject.endpoint == '/' }
+    end
+  end
+
+  describe '#header_defaults' do
+    context 'with parameters' do
+      Then do
+        subject.header_defaults == { 'Content-Type' => 'application/json' }
+      end
+    end
+
+    context 'without default header parameters' do
+      Given { yaml['parameter_defaults'].delete 'header' }
+      Then { subject.header_defaults == {} }
+    end
+
+    context 'without default parameters' do
+      Given { yaml.delete 'parameter_defaults' }
+      Then { subject.header_defaults == {} }
+    end
+  end
+
+  describe '#query_string_required' do
+    context 'with parameters' do
+      Then { subject.query_string_required == ['author', 'topic'] }
+    end
+
+    context 'without required query string parameters' do
+      Given { yaml['required_parameters'].delete 'query_string' }
+      Then { subject.query_string_required == [] }
+    end
+
+    context 'without required parameters' do
+      Given { yaml.delete 'required_parameters' }
+      Then { subject.query_string_required == [] }
+    end
+  end
+
+  describe '#header_required' do
+    context 'with required parameters' do
+      Then { subject.header_required == ['Authorization'] }
+    end
+
+    context 'without header required parameters' do
+      Given { yaml['required_parameters'].delete 'header' }
+      Then { subject.header_required == [] }
+    end
+
+    context 'without required parameters' do
+      Given { yaml.delete 'required_parameters' }
+      Then { subject.header_required == [] }
+    end
+  end
+
+  describe '#body_required' do
+    context 'with required parameters' do
+      Then { subject.body_required == ['title', 'text'] }
+    end
+
+    context 'without body required parameters' do
+      Given { yaml['required_parameters'].delete 'body' }
+      Then { subject.body_required == [] }
+    end
+
+    context 'without required parameters' do
+      Given { yaml.delete 'required_parameters' }
+      Then { subject.body_required == [] }
+    end
+  end
+
+  describe '#body_template' do
+    context 'with defined' do
+      Then do
+        expect(subject.body_template).to eq(<<~JSON.chomp)
+          {
+            "article_title": "<%= title %>",
+            "article_body": "<%= text %>"
+          }
+        JSON
+      end
+    end
+
+    context 'with default' do
+      Given { yaml.delete 'body_template' }
+      Then { subject.body_template == '' }
+    end
   end
 end
